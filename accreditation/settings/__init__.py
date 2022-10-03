d2c05ea67ec5be3e36a38866298b6d6aa5106c3c
Jefferson <jeffersonfarcar@gmail.com>
2022-10-03 11:39:17 -0500

    First Commit

--- accreditation/settings/__init__.py
+++ accreditation/settings/__init__.py
@@ -17,7 +17,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'accreditation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + '/templates']
+        'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
