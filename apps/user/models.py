68019f12a9fbefc97656d5451a94461c11543cc0
Jefferson <jeffersonfarcar@gmail.com>
2022-10-17 14:57:46 -0500

    HOTFIX: Icon field in Permission model Se arregló un bug en el campo icon del modelo permission ISSUE: #13

--- apps/user/models.py
+++ apps/user/models.py
@@ -21,7 +21,7 @@ def permission_path(instance, filename):
 
 class Permission(BaseAuditingModel):
     name = models.CharField(max_length=100, unique=True)
-    icon = models.ImageField(upload_to=permission_path, null=True, blank=True, verbose_name='icon_permission')
+    icon = models.CharField(max_length=100, null=True, blank=True)
     path = models.CharField(max_length=120, unique=True, blank=True, null=True)
     edit = models.BooleanField(default=True)
     fatherPermission = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
