0db824faf2e224463fa004a232f7b0d52b2b11e2
Arnold-MA <amolloa.147@gmail.com>
2022-11-13 22:56:00 -0500

    ADD: Folder model
    Se ha agregado el modelo de carpetas

--- apps/portafolio/models.py
+++ apps/portafolio/models.py
@@ -33,3 +33,18 @@ class Task(BaseAuditingModel):
 
     def __str__(self):
         return self.description
+
+
+class Folder(BaseAuditingModel):
+    name = models.CharField(max_length=120,unique=False, blank=False, null=False)
+    period_academic = models.ForeignKey(PeriodAcademic, on_delete=models.CASCADE, blank=False, null=False)
+    etapa = models.BigIntegerField() #phase = models.ForeignKey(Phase, on_delete=models.CASCADE, blank=False, null=False)
+
+    class Meta:
+        db_table = 'folder'
+        abstract = False
+        verbose_name = 'Folder'
+        verbose_name_plural = 'Folders'
+
+    def __str__(self):
+        return self.name
