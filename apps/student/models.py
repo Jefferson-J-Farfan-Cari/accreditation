4bfe7a06c282271a74106e8eeae83241d7070412
Jefferson <jeffersonfarcar@gmail.com>
2022-12-04 10:13:27 -0500

    HOTFIX: Fix model, serializer and view of leveldescription Se ha arreglado el modelo, serializador y vista del submódulo de level descripción

--- apps/student/models.py
+++ apps/student/models.py
@@ -60,23 +60,10 @@ class Level(BaseAuditingModel):
         return self.name
 
 
-class LevelDescription(BaseAuditingModel):
-    level = models.ForeignKey(Level, on_delete=models.CASCADE, blank=False, null=False)
-    description = models.CharField(max_length=380, unique=False, blank=False, null=False)
-
-    class Meta:
-        db_table = 'level_description'
-        abstract = False
-        verbose_name = 'Level Description'
-        verbose_name_plural = 'Levels Descriptions'
-
-    def __str__(self):
-        return self.description
-
-
 class Criteria(BaseAuditingModel):
     name = models.CharField(max_length=32, unique=False, blank=False, null=True)
-    student_result = models.ForeignKey(StudentResult, related_name='criteria', on_delete=models.CASCADE, blank=True, null=True)
+    student_result = models.ForeignKey(StudentResult, related_name='criteria', on_delete=models.CASCADE, blank=True,
+                                       null=True)
     description = models.CharField(max_length=380, unique=False, blank=False, null=False)
     levelSuggest = models.CharField(max_length=32, unique=False, blank=True)
 
@@ -85,6 +72,21 @@ class Criteria(BaseAuditingModel):
         abstract = False
         verbose_name = 'Criterion'
         verbose_name_plural = 'Criteria'
+
+    def __str__(self):
+        return self.description
+
+
+class LevelDescription(BaseAuditingModel):
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, blank=False, null=False)
+    description = models.CharField(max_length=380, unique=False, blank=False, null=False)
+    criteria = models.ForeignKey(Criteria, on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        db_table = 'level_description'
+        abstract = False
+        verbose_name = 'Level Description'
+        verbose_name_plural = 'Levels Descriptions'
 
     def __str__(self):
         return self.description
